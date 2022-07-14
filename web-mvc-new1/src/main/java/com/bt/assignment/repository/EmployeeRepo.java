0e5154a21b7184469d138d1ae4fef63cceac6a68
Rajanikant13 <98213514+Rajanikant13@users.noreply.github.com>
2022-07-14 14:26:07 +0530

    "Done"

---
+++ web-mvc-new1/src/main/java/com/bt/assignment/repository/EmployeeRepo.java
@@ -0,0 +1,13 @@
+package com.bt.assignment.repository;
+
+import com.bt.assignment.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeRepo {
+   public List<Employee> getAllEmployees();
+   public Employee getEmployeeById(int empId);
+   public Employee addEmployee(Employee emp);
+   public Employee updateEmployee(Employee emp);
+   public boolean deleteEmployee(int empId);
+}
